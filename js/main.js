f8ea6fcdde4d4f502b41d9a1015723c521414a1c
juanlendaro <juanlendaro@gmail.com>
2022-05-29 16:35:17 -0400

    primer desafio complementario

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let numero = prompt("¿Cúantos amigurumis querés comprar? Ingresa un número");
+while(numero != "ESC" ){
+    alert ("compraste " + numero+ " amigurumis");
+    numero = prompt("¿Querés seguir comprando? Ingresa otro número");
+}
