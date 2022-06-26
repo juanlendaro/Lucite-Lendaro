8e430c15f97871af8b8173a6280139a3302c70ee
juanlendaro <juanlendaro@gmail.com>
2022-06-25 23:31:42 -0400

    agregando eventos

--- js/main.js
+++ js/main.js
@@ -1,76 +1,8 @@
-// class Amigurumi {
-//     constructor(id, nombre, precio) {
-//         this.id = id;
-//         this.nombre = nombre;
-//         this.precio = precio;
-//     }
-// }
 
-// class Pedido {
-//     constructor(amigurumi, cantidad) {
-//         this.amigurumis = amigurumi;
-//         this.cantidad = cantidad;
-//     }
-// }
-
-// const amigurumi1 = new Amigurumi("1", "Oso", 400);
-// const amigurumi2 = new Amigurumi("2", "Dinosaurio", 600);
-// const amigurumi3 = new Amigurumi("3", "Conejo", 200);
-// const amigurumi4 = new Amigurumi("4", "Koala", 150);
-
-// const amigurumis = [amigurumi1, amigurumi2, amigurumi3, amigurumi4]
-// const carrito = []
-
-// function agregarAmigurumiAlCarrito(id) {
-//     const amigurumiEncontrado = amigurumis.find(amigurumi => amigurumi.id == id);
-//     if (amigurumiEncontrado == undefined) {
-//         alert("No tenemos este amigurumi por el momento");
-//     } else {
-//         const cantidad = prompt("Ingresá cuantas unidades de " + amigurumiEncontrado.nombre)
-//         const nuevoPedido = new Pedido(amigurumiEncontrado, cantidad)
-//         carrito.push(nuevoPedido);
-//     }
-// }
-
-// function mostrarCarritoActual() {
-//     alert("Compraste " + nuevoPedido);
-// }
-
-// function mostrarAmigurumis() {
-//     let mostrarMenu = "¿Que amigurumi querés?\n";
-//     amigurumis.forEach(amigurumi => {
-//         mostrarMenu += amigurumi.id + ") " + amigurumi.nombre + " - " + amigurumi.precio + "\n";
-//     })
-//     let respuesta = prompt(mostrarMenu);
-//     return respuesta;
-// }
-
-
-// function confirmarCompra() {
-//     const respuesta = prompt("¿Querés comprar un amigurumi? Escribí el número correspondiente\n1) Si \n2) No");
-//     if (respuesta == "1") {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// function comprarAmigurumi() {
-//     while (confirmarCompra()) {
-//         let amigurumiElegido = mostrarAmigurumis();
-//         agregarAmigurumiAlCarrito(amigurumiElegido);
-//         mostrarCarritoActual();
-//     }
-//     if (respuesta = 2) {
-//         gracias();
-//     }
-
-//     function gracias() {
-//         alert("Gracias por visitar Lucite");
-//     }
-// }
-
-// comprarAmigurumi();
+function agregarAlCarrito(id) {
+    let productoAgregar = stock1.find(item => item.id === id) || stock2.find(item => item.id === id);
+    console.log(productoAgregar);
+}
 
 function mostrarStock1(stock1) {
 
@@ -85,10 +17,16 @@ function mostrarStock1(stock1) {
                 <h5 class="card-title text-center">"${amigurumi.nombre}"</h5>
                 <p class="card-text">${amigurumi.desc}</p>
                 <p>Precio: $${amigurumi.precio}</p>
-                <a href="#" class="btn btn-primary">Comprar</a>
+                <a id= "boton ${amigurumi.id}" class="btn btn-primary">Comprar</a>
               </div>
              `;
         columna1.appendChild(divCard);
+
+        const btnAgregar = document.getElementById(`boton ${amigurumi.id}`);
+
+        btnAgregar.addEventListener("click", () => {
+            agregarAlCarrito(amigurumi.id);
+        })
     })
 }
 
@@ -105,10 +43,16 @@ function mostrarStock2(stock2) {
                 <h5 class="card-title text-center">"${amigurumi.nombre}"</h5>
                 <p class="card-text">${amigurumi.desc}</p>
                 <p>Precio: $${amigurumi.precio}</p>
-                <a href="#" class="btn btn-primary">Comprar</a>
+                <a id= "boton ${amigurumi.id}" class="btn btn-primary">Comprar</a>
               </div>
              `;
         columna2.appendChild(divCard);
+
+        const btnAgregar = document.getElementById(`boton ${amigurumi.id}`);
+
+        btnAgregar.addEventListener("click", () => {
+            agregarAlCarrito(amigurumi.id);
+        })
     })
 }
 
